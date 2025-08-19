174674f08ea9752fae6b7b1c872713f8d9e8623a
Nilay Türker <nilaytr89@gmail.com>
2025-08-19 23:18:15 +0300

    redux edit

---
+++ src/redux/filters/filtersSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    location: '',
+    transmission: '',
+    price: '',
+    rating: '',
+    engine: '',
+    form: '',
+    ac: false,
+    kitchen: false,
+    tv: false,
+    bathroom: false,
+    radio: false,
+    refrigerator: false,
+    microwave: false,
+    gas: false,
+    water: false
+};
+
+const filtersSlice = createSlice({
+    name: 'filters',
+    initialState,
+    reducers: {
+        updateFilters(state, action) {
+            return { ...state, ...action.payload }
+        },
+        resetFilters() {
+            return { ...initialState }
+        },
+    },
+});
+
+export const { updateFilters, resetFilters } = filtersSlice.actions;
+
+export default filtersSlice.reducer;
